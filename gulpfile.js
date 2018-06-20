0ed39d75103acdb154d989b13dd3b31e630c38ea
Ryan Phillips <vencermedia@gmail.com>
2018-06-20 13:40:42 -0500

    Configured Email Template For Testing
    updated package.json file, updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,31 @@
 var mail = require('./')
-var mailInfo = require('./test/mail-info')  // private
+  // private
 var gulp = require('gulp')
+
+var mail = require('gulp-mail');
+
+var smtpInfo = {
+  auth: {
+    user: 'ryan@hotcoffey.com',
+    pass: 'Swingline'
+  },
+  host: 'server.hotcoffeydesign.com',
+  secureConnection: true,
+  port: 465
+};
+
+gulp.task('mail', function () {
+  return gulp.src('./test/drip.html')
+    .pipe(mail({
+      subject: 'Surprise!?',
+      to: [
+        'ryan@hotcoffeydesign.com'
+      ],
+      from: 'ryan@hotcoffey.com',
+      smtp: smtpInfo
+    }));
+});
+
 
 gulp.task('test', function(){
   // send mail
